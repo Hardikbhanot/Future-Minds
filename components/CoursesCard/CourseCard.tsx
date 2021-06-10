ca748329d6461cfcccb75d1cac8d3ac85294c5da
chetnagrover00 <65505815+chetnagrover00@users.noreply.github.com>
2021-06-10 15:25:05 +0530

    card markup written,styles pending

--- components/CoursesCard/CourseCard.tsx
+++ components/CoursesCard/CourseCard.tsx
@@ -7,17 +7,49 @@ interface courseFields {
   discription: string;
   noOfCourses: number;
   noOfStudents: number;
+  courseLevel: string;
+  price: number;
+
 }
 
 interface pageProps {
   course: courseFields;
 }
 
-const CourseCard = (props: pageProps) => {
+const CourseCard = ({ course }: pageProps) => {
   return (
-    <section className={styles.course_card}>
-      <h1>{props.course.courseName}</h1>
-    </section>
+
+    <div className={styles.card} >
+
+      <div className={styles.imgCard}>
+        <p className={styles.levels}> {course.courseLevel}</p>
+        <button> Bookmark </button>
+
+        <div className={styles.prices} />
+        <p className={styles.priceValue}><strong>{course.price}</strong> Including GST</p>
+
+
+        <img src={course.courseImg}
+          alt="Course name"
+          className="card_img"
+        />
+
+      </div>
+      <div className={styles.dataCard}>
+
+        <div className={styles.starRating} />
+
+        <h1 className={styles.courseName}>{course.courseName}</h1>
+        <p className={styles.courseDescription}>{course.discription}</p>
+        <div className={styles.cardFooter}>
+          <p>Lessons</p>
+          <p>Students</p>
+        </div>
+      </div>
+
+    </div>
+
+
   );
 };
 
